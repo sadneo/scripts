ee8b9f49153ab4cfb4e3b60347f0d3e11f434d84
sadneo <70412323+sadneo@users.noreply.github.com>
2020-11-29 13:25:17 -0500

    Update BecomeAStand.lua

--- BecomeAStand.lua
+++ BecomeAStand.lua
@@ -1,24 +1,40 @@
 -- Made by sadneo --
-local TARGET_NAME = "TARGET NAME HERE"
+
+local ScreenGui = Instance.new("ScreenGui")
+local TextBox = Instance.new("TextBox")
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+TextBox.Parent = ScreenGui
+TextBox.AnchorPoint = Vector2.new(0.5, 0.5)
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextBox.Size = UDim2.new(0, 200, 0, 50)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.Text = "Write Target Name Here"
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextSize = 14.000
 
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
 local UserInputService = game:GetService("UserInputService")
 
-local target = Players:FindFirstChild(TARGET_NAME)
 local player = Players.LocalPlayer
 
 local summoned = false
 local barraging = false
 local punchAnimationTrack = nil
 local offsetCFrame = CFrame.new(3, 1, 3)
+local tweenInfo = TweenInfo.new(.1)
 
 
 
 -- set animation yes
 if player.Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-    local punchAnimation = Instance.new("Animation")
+    local punchAnimation = Instance.new("Animation", player.Character)
     punchAnimation.AnimationId = "rbxassetid://2515090838"
     player.Character.Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=657595757"
     punchAnimationTrack = player.Character.Humanoid:LoadAnimation(punchAnimation)
@@ -47,7 +63,7 @@ end)
 
 RunService.Stepped:Connect(function()
     if summoned == true then
-        player.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame:ToWorldSpace(offsetCFrame)
+        player.Character.HumanoidRootPart.CFrame = Players:FindFirstChild(TextBox.Text).Character.HumanoidRootPart.CFrame:ToWorldSpace(offsetCFrame)
         player.Character.Humanoid:ChangeState(11)
     end
 end)
