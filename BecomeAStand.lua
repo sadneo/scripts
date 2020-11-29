5bc5cb17049c359b8abb6c895855407556b51c7b
sadneo <70412323+sadneo@users.noreply.github.com>
2020-11-28 21:00:48 -0500

    Update BecomeAStand.lua

--- BecomeAStand.lua
+++ BecomeAStand.lua
@@ -8,33 +8,41 @@ local UserInputService = game:GetService("UserInputService")
 
 local target = Players:FindFirstChild(TARGET_NAME)
 local player = Players.LocalPlayer
-local char = player.Character
 
 local summoned = false
+local barraging = false
 local offsetCFrame = CFrame.new(3, 1, 3)
 
 -- set animation yes
-if char.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-    char.Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=657595757"
+if player.Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
+    player.Character.Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=657595757"
 end
 
 local function punch()
 
 end
 
-UserInputService.InputBegan:Connect(function(input)
+UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+    if gameProcessedEvent then return end
     if input.KeyCode == Enum.KeyCode.Q then
         summoned = not summoned
     elseif input.KeyCode == Enum.KeyCode.E then
-
+        barraging = true
     elseif input.KeyCode == Enum.KeyCode.R then
-           
+        punch()
     end
 end)
 
+UserInputService.InputEnded:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.E then
+        barraging = false
+    end
+end)
+
+
 RunService.Stepped:Connect(function()
     if summoned == true then
-        char.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame:ToWorldSpace(offsetCFrame)
-        char.Humanoid:ChangeState(11)
+        player.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame:ToWorldSpace(offsetCFrame)
+        player.Character.Humanoid:ChangeState(11)
     end
 end)
